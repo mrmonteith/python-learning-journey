1c2c5806cf6df4cf4a42338e9b58f508d835238f
Michael R. Monteith <mrmonteith@users.noreply.github.com>
2025-12-01 06:29:21 -0500

    Added variables and input/output examples

---
+++ 01_basics/math_operations.py
@@ -0,0 +1,6 @@
+a = 10
+b = 3
+print(f"Addition: {a + b}")
+print(f"Division: {a / b}")
+print(f"Floor Division: {a // b}")
+print(f"Power: {a ** b}")
---
+++ 01_basics/variables.py
@@ -0,0 +1,3 @@
+name = "Michael"
+age = 42
+print(f"My name is {name} and I am {age} years old.")
